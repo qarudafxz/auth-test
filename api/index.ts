d3758b9ad4e0461652b5c95c2e93de1e2d45078e
qaruda <70809588+francistinao@users.noreply.github.com>
2023-09-12 10:10:31 +0800

    this is a test for auth

---
+++ api/index.ts
@@ -0,0 +1,67 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import Pool from "pg";
+
+dotenv.config();
+
+// type Student = {
+// 	student_id: string;
+// 	name: string;
+// };
+
+const app = express();
+app.use(express.json());
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	})
+);
+
+const qrDb = new Pool.Pool({
+	user: "postgres",
+	host: "localhost",
+	database: "qr-test",
+	password: "garuda06242008",
+	port: 5432,
+});
+
+qrDb.connect().then(() => console.log("Connected to database"));
+
+app.post("/api/persist", async (req: Request, res: Response) => {
+	const { stud_id, name } = req.body;
+
+	try {
+		const findStudent = await qrDb.query(
+			`SELECT * FROM tbl_student WHERE stud_id = '${stud_id}'`
+		);
+
+		if (findStudent.rows.length > 0) {
+			const ifExist = findStudent.rows.find(
+				(student) => student.stud_id === stud_id
+			);
+
+			if (ifExist) {
+				return res.status(400).json({
+					message: "Student already exist",
+					student: ifExist,
+				});
+			}
+		}
+
+		const newStudent = await qrDb.query(
+			`INSERT INTO tbl_student (stud_id, name) VALUES ('${stud_id}', '${name}') RETURNING *`
+		);
+
+		res.status(200).json({
+			message: "Student added successfully",
+			student: newStudent.rows[0],
+		});
+	} catch (err) {
+		res.status(500).json({ err, message: "Something went wrong" });
+	}
+});
+
+app.listen(3000, () => console.log(`Server is running on port 3000`));
---
+++ src/App.tsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import bscs from "../data/bscs.json";
+import bsit from "../data/bsit.json";
+import bsis from "../data/bsis.json";
+import axios from "axios";
+
+import QrScanner from "qr-scanner";
+
+type Student = {
+	studentNo: string;
+	name: string;
+};
+
+const App: React.FC = () => {
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const [studentId, setStudentId] = useState<string | null>(null);
+	const [studentInfo, setStudentInfo] = useState<string>("");
+
+	useEffect(() => {
+		let qrScanner: QrScanner;
+
+		QrScanner.hasCamera().then(() => {
+			if (!videoRef.current) return;
+
+			qrScanner = new QrScanner(
+				videoRef.current,
+				(result: QrScanner.ScanResult) => {
+					if (result) {
+						setStudentId(result.data);
+					}
+				},
+				{
+					returnDetailedScanResult: true,
+					preferredCamera: 1,
+					highlightScanRegion: true,
+					maxScansPerSecond: 3,
+					singleChannel: false,
+					calculateScanRegion: (v: HTMLVideoElement) => {
+						const heightRegionSize = Math.round(
+							0.7 * Math.min(v.videoWidth, v.videoHeight)
+						);
+						const widthRegionSize = Math.round(0.4 * v.videoWidth);
+
+						const region: QrScanner.ScanRegion = {
+							x: Math.round((v.videoWidth - widthRegionSize) / 2),
+							y: Math.round((v.videoHeight - heightRegionSize) / 2),
+							width: widthRegionSize,
+							height: heightRegionSize,
+						};
+
+						return region;
+					},
+				}
+			);
+
+			qrScanner.start();
+
+			return () => {
+				if (qrScanner) qrScanner.destroy();
+			};
+		});
+	}, []);
+
+	// const persistStudentInfo = useCallback(async (studentInfo: string) => {
+	// 	await fetch("https://localhost:3001/", {
+	// 		method: "POST",
+	// 		body: JSON.stringify({ studentInfo }),
+	// 	}).then((res) => {
+	// 		console.log(res);
+	// 	});
+	// });
+
+	const filterStudent = () => {
+		const bscsStudent = bscs.find(
+			(student: Student) => student.studentNo === studentId
+		);
+		const bsitStudent = bsit.find(
+			(student: Student) => student.studentNo === studentId
+		);
+		const bsisStudent = bsis.find(
+			(student: Student) => student.studentNo === studentId
+		);
+
+		if (bscsStudent) {
+			setStudentInfo(bscsStudent.name);
+		} else if (bsitStudent) {
+			setStudentInfo(bsitStudent.name);
+		} else if (bsisStudent) {
+			setStudentInfo(bsisStudent.name);
+		} else {
+			setStudentInfo("Student not found");
+		}
+	};
+
+	const persistInfo = useCallback(
+		async (studentId: string, studentInfo: string) => {
+			console.log;
+			try {
+				const response = await fetch("http://localhost:3000/api/persist", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						stud_id: studentId,
+						name: studentInfo,
+					}),
+				});
+				const data = await response.json();
+				alert(data.message);
+			} catch (error) {
+				console.error(error);
+			}
+		},
+		[studentId, studentInfo]
+	);
+
+	useEffect(() => {
+		//search to all json
+
+		//if found, display the name of the student
+
+		//if not found, display "Student not found"
+		filterStudent();
+
+		persistInfo(studentId!, studentInfo!);
+	}, [studentId, studentInfo]);
+
+	return (
+		<div>
+			<h1 className='font-bold'>QR Scanner Implementation</h1>
+			<video ref={videoRef}></video>
+			<h1 className='font-bold text-2xl'>
+				Student Id: {studentId ? studentId : "Scanning..."}
+			</h1>
+			<h1>Student Name: {studentInfo}</h1>
+		</div>
+	);
+};
+
+export default App;
